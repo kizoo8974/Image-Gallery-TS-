ed5f846e3fdd9743c8cbcbe8953968d3bfc02cbe
Joon Kim <kizoo8974@gamil.com>
2023-06-16 13:25:54 +0900

    Using react-dropzone

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
-import React, { useRef, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ImageBox from './components/imageBox';
+import {useDropzone} from 'react-dropzone'
 
 function App() {
   const inpRef = useRef<HTMLInputElement>(null);
 
   const [imageList, setImageList] = useState<string[]>([])
 
- 
+  const onDrop = useCallback((acceptedFiles: any) => {
+      console.log(acceptedFiles)
+       
+        if (acceptedFiles.length) {
+
+            for (const file of acceptedFiles) {
+              console.log(file.name)
+
+            const reader = new FileReader();
+
+            reader.readAsDataURL(file)
+            reader.onloadend = (event) => {
+              setImageList(prev => [...prev, event.target?.result as string])
+            }
+
+            
+            }
+            
+            }
+          
+  }, [])
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({onDrop})
 
   return (
     <div className='container'>
@@ -23,31 +46,22 @@ function App() {
           </div>
         }
           
-          <input type="file" ref={inpRef} 
-          onChange={(event) => {
-            
-            if(event.currentTarget.files?.[0]){
-
-            const file = event.currentTarget.files[0]
-
-            console.log(file.name)
-
-            const reader = new FileReader();
-
-            reader.readAsDataURL(file)
-            reader.onloadend = (event) => {
-              setImageList(prev => [...prev, event.target?.result as string])
-            }
-            
-            }
-          }}/>
+          
           {
             imageList.map((el, idx) =><ImageBox key={el + idx} src={el} />)
           }
 
-          <div className ="plus-box" onClick={()=> {
-            inpRef.current?.click()
-          } }>
+          <div className ="plus-box" 
+          {...getRootProps()}
+          // onClick={()=> {
+          //   inpRef.current?.click()
+          // } }
+          >
+            
+            <input type="file" ref={inpRef} 
+            {...getInputProps()}
+          
+          />
             +
 
           </div>
